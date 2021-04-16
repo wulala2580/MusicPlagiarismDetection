8f8f8f45f7bef0ae7598a3bc83dea08b7395881e
wulala2580 <635033547@qq.com>
2021-04-16 23:17:57 +0800

    Add files via upload

---
+++ CodesandResults/test.py
@@ -0,0 +1,5 @@
+from Classes import *
+path = data_path = 'D:/DaSanXia/CS222-Music-Plagiarism-master/dataset/case15_match.npy'
+music = Music(path)
+note_list = music.original_notes_list
+print(note_list)
