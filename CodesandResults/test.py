9ec74f525f476aea0f19a87a3f4d7898a6f2c746
wulala2580 <635033547@qq.com>
2021-04-16 23:19:12 +0800

    Delete CodesandResults directory

--- CodesandResults/test.py
+++
@@ -1,5 +0,0 @@
-from Classes import *
-path = data_path = 'D:/DaSanXia/CS222-Music-Plagiarism-master/dataset/case15_match.npy'
-music = Music(path)
-note_list = music.original_notes_list
-print(note_list)
